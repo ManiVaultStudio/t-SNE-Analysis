7ec3753dcda1eadf447b73094c2f6aed83c92bdb
Julian Thijssen <Nimthora@gmail.com>
2019-10-23 14:12:05 +0200

    Add P matrix saving

--- src/TsneAnalysis.cpp
+++ src/TsneAnalysis.cpp
@@ -11,6 +11,8 @@
 
 #include <QWindow>
 #include <QOpenGLContext>
+#include <chrono>
+#include <fstream>
 
 class OffscreenBuffer : public QWindow
 {
@@ -82,6 +84,52 @@ void TsneAnalysis::computeGradientDescent()
     embed();
 }
 
+template <typename sparse_scalar_matrix_type, class output_stream_type>
+void saveSparseMatrix(const sparse_scalar_matrix_type& matrix, output_stream_type& stream) {
+    typedef float io_scalar_type;
+    typedef uint32_t io_unsigned_int_type;
+
+    //number of rows first
+    io_unsigned_int_type num_rows = static_cast<io_unsigned_int_type>(matrix.size());
+    stream.write(reinterpret_cast<char*>(&num_rows), sizeof(io_unsigned_int_type));
+    for (int j = 0; j < num_rows; ++j) {
+        //number of elements in the current row
+        io_unsigned_int_type num_elems = static_cast<io_unsigned_int_type>(matrix[j].size());
+
+        stream.write(reinterpret_cast<char*>(&num_elems), sizeof(io_unsigned_int_type));
+        for (auto& elem : matrix[j]) {
+            io_unsigned_int_type id = static_cast<io_unsigned_int_type>(elem.first);
+            io_scalar_type v = static_cast<io_scalar_type>(elem.second);
+            stream.write(reinterpret_cast<char*>(&id), sizeof(io_unsigned_int_type));
+            stream.write(reinterpret_cast<char*>(&v), sizeof(io_scalar_type));
+        }
+    }
+}
+
+template <typename sparse_scalar_matrix_type, class output_stream_type>
+void loadSparseMatrix(sparse_scalar_matrix_type& matrix, output_stream_type& stream) {
+    typedef float io_scalar_type;
+    typedef uint32_t io_unsigned_int_type;
+
+    //number of rows first
+    io_unsigned_int_type num_rows;
+    stream.read(reinterpret_cast<char*>(&num_rows), sizeof(io_unsigned_int_type));
+    matrix.clear();
+    matrix.resize(num_rows);
+    for (int j = 0; j < num_rows; ++j) {
+        //number of elements in the current row
+        io_unsigned_int_type num_elems;
+        stream.read(reinterpret_cast<char*>(&num_elems), sizeof(io_unsigned_int_type));
+        for (int i = 0; i < num_elems; ++i) {
+            io_unsigned_int_type id;
+            io_scalar_type v;
+            stream.read(reinterpret_cast<char*>(&id), sizeof(io_unsigned_int_type));
+            stream.read(reinterpret_cast<char*>(&v), sizeof(io_scalar_type));
+            matrix[j][id] = v;
+        }
+    }
+}
+
 void TsneAnalysis::initTSNE(const std::vector<float>& data, const int numDimensions)
 {
     unsigned int numPoints = data.size() / numDimensions;
@@ -93,35 +141,45 @@ void TsneAnalysis::initTSNE(const std::vector<float>& data, const int numDimensi
     
     /// Computation of the high dimensional similarities
     qDebug() << "Output allocated.";
+    hdi::dr::HDJointProbabilityGenerator<float>::sparse_scalar_matrix_type distributions;
+    std::ifstream input_file("raw_mnist_P", std::ios::binary);
+    if (input_file.is_open())
     {
-        hdi::dr::HDJointProbabilityGenerator<float>::Parameters probGenParams;
-        probGenParams._perplexity = _perplexity;
-        probGenParams._perplexity_multiplier = 1;
-        probGenParams._num_trees = _numTrees;
-        probGenParams._num_checks = _numChecks;
-
-        qDebug() << "tSNE initialized.";
-
-        _probabilityDistribution.clear();
-        _probabilityDistribution.resize(numPoints);
-        qDebug() << "Sparse matrix allocated.";
-
-        qDebug() << "Computing high dimensional probability distributions.. Num dims: " << numDimensions << " Num data points: " << numPoints;
-        hdi::dr::HDJointProbabilityGenerator<float> probabilityGenerator;
-        double t = 0.0;
-        {
-            hdi::utils::ScopedTimer<double> timer(t);
-            probabilityGenerator.computeJointProbabilityDistribution(_inputData.getDataNonConst().data(), numDimensions, numPoints, _probabilityDistribution, probGenParams);
-        }
-        qDebug() << "Probability distributions calculated.";
-        qDebug() << "================================================================================";
-        qDebug() << "A-tSNE: Compute probability distribution: " << t / 1000 << " seconds";
-        qDebug() << "--------------------------------------------------------------------------------";
+        loadSparseMatrix(_probabilityDistribution, input_file);
     }
+
+    //{
+    //    hdi::dr::HDJointProbabilityGenerator<float>::Parameters probGenParams;
+    //    probGenParams._perplexity = _perplexity;
+    //    probGenParams._perplexity_multiplier = 1;
+    //    probGenParams._num_trees = _numTrees;
+    //    probGenParams._num_checks = _numChecks;
+
+    //    qDebug() << "tSNE initialized.";
+
+    //    _probabilityDistribution.clear();
+    //    _probabilityDistribution.resize(numPoints);
+    //    qDebug() << "Sparse matrix allocated.";
+
+    //    qDebug() << "Computing high dimensional probability distributions.. Num dims: " << numDimensions << " Num data points: " << numPoints;
+    //    hdi::dr::HDJointProbabilityGenerator<float> probabilityGenerator;
+    //    double t = 0.0;
+    //    {
+    //        hdi::utils::ScopedTimer<double> timer(t);
+    //        probabilityGenerator.computeJointProbabilityDistribution(_inputData.getDataNonConst().data(), numDimensions, numPoints, _probabilityDistribution, probGenParams);
+    //    }
+    //    qDebug() << "Probability distributions calculated.";
+    //    qDebug() << "================================================================================";
+    //    qDebug() << "A-tSNE: Compute probability distribution: " << t / 1000 << " seconds";
+    //    qDebug() << "--------------------------------------------------------------------------------";
+    //}
+    //std::ofstream stream("probs", std::ios::binary);
+    //saveSparseMatrix(_probabilityDistribution, stream);
 }
 
 void TsneAnalysis::initGradientDescent()
 {
+  qDebug() << "Initializing gradient descent";
     _continueFromIteration = 0;
 
     _isTsneRunning = true;
@@ -134,15 +192,16 @@ void TsneAnalysis::initGradientDescent()
     tsneParams._exponential_decay_iter = 150;
     tsneParams._exaggeration_factor = 10 + _inputData.getNumPoints() / 5000.;
     _A_tSNE.setTheta(std::min(0.5, std::max(0.0, (_inputData.getNumPoints() - 1000.0)*0.00005)));
-
+    qDebug() << "Initializing2 gradient descent";
     // Create a context local to this thread that shares with the global share context
     offBuffer = new OffscreenBuffer();
     offBuffer->initialize();
-
+    qDebug() << "Initializing3 gradient descent";
     // Initialize GPGPU-SNE
     offBuffer->bindContext();
+    qDebug() << "Initializing4 gradient descent";
     _GPGPU_tSNE.initialize(_probabilityDistribution, &_embedding, tsneParams);
-    
+    qDebug() << "Initializing5 gradient descent";
     copyFloatOutput();
 }
 
@@ -150,15 +209,17 @@ void TsneAnalysis::initGradientDescent()
 void TsneAnalysis::embed()
 {
     double elapsed = 0;
-    double t = 0;
+
     {
         qDebug() << "A-tSNE: Computing gradient descent..\n";
+        hdi::utils::ScopedTimer<double> timer(elapsed);
+
         _isGradientDescentRunning = true;
 
         // Performs gradient descent for every iteration
         for (int iter = 0; iter < _iterations; ++iter)
         {
-            hdi::utils::ScopedTimer<double> timer(t);
+            
             if (!_isGradientDescentRunning)
             {
                 _continueFromIteration = iter;
@@ -168,16 +229,12 @@ void TsneAnalysis::embed()
             // Perform a GPGPU-SNE iteration
             _GPGPU_tSNE.doAnIteration();
 
-            if (iter > 0 && iter % 10 == 0)
+            if (iter > 0 && iter % 2 == 0)
             {
                 copyFloatOutput();
                 emit newEmbedding();
+                //std::this_thread::sleep_for(std::chrono::milliseconds(10));
             }
-
-            if (t > 1000)
-                qDebug() << "Time: " << t;
-
-            elapsed += t;
         }
         offBuffer->releaseContext();
 
