8b3b910ec755095b06be0f5e7d216cf4b3fc731e
Niels Dekker <N.Dekker@lumc.nl>
2019-12-01 20:55:55 +0100

    ENH: Improve size + alignment Dimension Selection table cells + headers

--- src/DimensionSelectionWidget.cpp
+++ src/DimensionSelectionWidget.cpp
@@ -273,6 +273,22 @@ namespace hdps
             return ui;
         }())
         {
+            QHeaderView *const verticalHeader = _ui.tableView->verticalHeader();
+
+            if (verticalHeader != nullptr)
+            {
+                // Reduce height of cells by 33%. The cells were really too high, originally!
+                verticalHeader->setSectionResizeMode(QHeaderView::Fixed);
+                const auto originalDefaultSectionSize = verticalHeader->defaultSectionSize();
+                verticalHeader->setDefaultSectionSize((2 * originalDefaultSectionSize)/3);
+            }
+
+            QHeaderView *const horizontalHeader = _ui.tableView->horizontalHeader();
+            if (horizontalHeader != nullptr)
+            {
+                // Especially the header text looks much better, when left-aligned.
+                horizontalHeader->setDefaultAlignment(Qt::AlignLeft);
+            }
             updateLabel();
 
             connectButtonClicked(*_ui.loadPushButton, [this, &widget]
