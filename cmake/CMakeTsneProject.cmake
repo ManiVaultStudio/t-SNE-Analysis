71e57aebe71719f8a5ddd234350e4434024d716a
Julian Thijssen <Nimthora@gmail.com>
2021-07-31 19:11:51 +0200

    Clean up CMake files

--- cmake/CMakeTsneProject.cmake
+++ cmake/CMakeTsneProject.cmake
@@ -1,51 +1,71 @@
-# Tsne Plugin
+# -----------------------------------------------------------------------------
+# T-SNE Plugin Target
+# -----------------------------------------------------------------------------
 set(TSNE_PLUGIN "TsneAnalysisPlugin")
 
-project(${TSNE_PLUGIN})
-
+# -----------------------------------------------------------------------------
+# Source files
+# -----------------------------------------------------------------------------
 add_subdirectory(tSNE/src)
-# Normalize the incoming install path
-#file(TO_CMAKE_PATH $ENV{HDPS_INSTALL_DIR} INSTALL_DIR)
 
 source_group(DimensionSelection FILES ${DIMENSION_SELECTION_SOURCES})
 source_group(Actions FILES ${TSNE_ACTIONS_SOURCES} ${DIMENSION_SELECTION_ACTION_SOURCES})
 source_group(Tsne FILES ${TSNE_PLUGIN_SOURCES})
-find_package(OpenGL REQUIRED)
 
-QT5_WRAP_UI(UI_HEADERS ${UI_FILES})
-
+# -----------------------------------------------------------------------------
+# CMake Target
+# -----------------------------------------------------------------------------
 add_library(${TSNE_PLUGIN} SHARED
     ${DIMENSION_SELECTION_SOURCES}
-	${TSNE_ACTIONS_SOURCES}
-	${DIMENSION_SELECTION_ACTION_SOURCES}
+    ${TSNE_ACTIONS_SOURCES}
+    ${DIMENSION_SELECTION_ACTION_SOURCES}
     ${TSNE_COMMON_SOURCES}
     ${TSNE_PLUGIN_SOURCES}
     ${UI_FILES}
 )
 
-include_directories("${INSTALL_DIR}/$<CONFIGURATION>/include/")
-set_HDILib_project_includes()
-include_directories("Common")
-set_flann_project_includes()
+# -----------------------------------------------------------------------------
+# Target include directories
+# -----------------------------------------------------------------------------
+# For inclusion of Qt generated ui_DimensionSelectionWidget.h
+target_include_directories(${TSNE_PLUGIN} PRIVATE ${PROJECT_BINARY_DIR})
 
+# Include HDPS core headers
+target_include_directories(${TSNE_PLUGIN} PRIVATE "${INSTALL_DIR}/$<CONFIGURATION>/include/")
+
+target_include_directories(${TSNE_PLUGIN} PRIVATE "Common")
+
+set_HDILib_project_includes(${TSNE_PLUGIN})
+set_flann_project_includes(${TSNE_PLUGIN})
+
+# -----------------------------------------------------------------------------
+# Target properties
+# -----------------------------------------------------------------------------
 # Request C++17, in order to use std::for_each_n with std::execution::par_unseq.
-set_property(TARGET ${TSNE_PLUGIN} PROPERTY CXX_STANDARD 17)
+set_target_properties(${TSNE_PLUGIN} PROPERTIES CXX_STANDARD 17)
 
 target_compile_definitions(${TSNE_PLUGIN} PRIVATE QT_MESSAGELOGCONTEXT)
 
+# -----------------------------------------------------------------------------
+# Target library linking
+# -----------------------------------------------------------------------------
 target_link_libraries(${TSNE_PLUGIN} Qt5::Widgets)
 target_link_libraries(${TSNE_PLUGIN} Qt5::WebEngineWidgets)
 if(MSVC)
-	set(LIB_SUFFIX "${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    set(LIB_SUFFIX "${CMAKE_STATIC_LIBRARY_SUFFIX}")
 else()
-	set(LIB_SUFFIX "${CMAKE_SHARED_LIBRARY_SUFFIX}")
+    set(LIB_SUFFIX "${CMAKE_SHARED_LIBRARY_SUFFIX}")
 endif()
+
 target_link_libraries(${TSNE_PLUGIN} "${INSTALL_DIR}/$<CONFIGURATION>/lib/${CMAKE_SHARED_LIBRARY_PREFIX}HDPS_Public${LIB_SUFFIX}")
 target_link_libraries(${TSNE_PLUGIN} "${INSTALL_DIR}/$<CONFIGURATION>/lib/${CMAKE_SHARED_LIBRARY_PREFIX}PointData${LIB_SUFFIX}")
 target_link_libraries(${TSNE_PLUGIN} ${OPENGL_LIBRARIES})
-set_flann_project_link_libraries()
-set_HDILib_project_link_libraries()
+set_flann_project_link_libraries(${TSNE_PLUGIN})
+set_HDILib_project_link_libraries(${TSNE_PLUGIN})
 
+# -----------------------------------------------------------------------------
+# Target installation
+# -----------------------------------------------------------------------------
 install(TARGETS ${TSNE_PLUGIN}
    RUNTIME DESTINATION "${INSTALL_DIR}/$<CONFIGURATION>/Plugins" COMPONENT HSNE_SHAREDLIB
 )
