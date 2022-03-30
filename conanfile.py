316acbcf0552cc711eba7813a98d45e276598235
bldrvnlw <b.van_lew@lumc.nl>
2022-03-30 15:52:29 +0200

    Qt requirement is inherited from core
    Remove it from this because of possible incompatibility

--- conanfile.py
+++ conanfile.py
@@ -34,8 +34,6 @@ class SNEAnalysesConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": True, "fPIC": True}
 
-    requires = "qt/5.15.1@lkeb/stable"
-
     scm = {
         "type": "git",
         "subfolder": "hdps/t-SNE-Analysis",
