d587775587eacfba5237d32faccb20611599ac5b
Alexander Vieth <a.vieth@tudelft.nl>
2024-07-22 10:21:07 +0200

    Use find_package for ManiVault

--- cmake/CMakeTsneProject.cmake
+++ cmake/CMakeTsneProject.cmake
@@ -27,9 +27,7 @@ add_library(${TSNE_PLUGIN} SHARED
 # -----------------------------------------------------------------------------
 # Target include directories
 # -----------------------------------------------------------------------------
-# Include ManiVault core headers
-target_include_directories(${TSNE_PLUGIN} PRIVATE "${MV_INSTALL_DIR}/$<CONFIGURATION>/include/")
-
+target_include_directories(${TSNE_PLUGIN} PRIVATE "${ManiVault_INCLUDE_DIR}")
 target_include_directories(${TSNE_PLUGIN} PRIVATE "src/Common")
 
 set_HDILib_project_includes(${TSNE_PLUGIN})
@@ -54,15 +52,8 @@ endif()
 target_link_libraries(${TSNE_PLUGIN} PRIVATE Qt6::Widgets)
 target_link_libraries(${TSNE_PLUGIN} PRIVATE Qt6::WebEngineWidgets)
 
-set(MV_LINK_PATH "${MV_INSTALL_DIR}/$<CONFIGURATION>/lib")
-set(PLUGIN_LINK_PATH "${MV_INSTALL_DIR}/$<CONFIGURATION>/$<IF:$<CXX_COMPILER_ID:MSVC>,lib,Plugins>")
-set(MV_LINK_SUFFIX $<IF:$<CXX_COMPILER_ID:MSVC>,${CMAKE_LINK_LIBRARY_SUFFIX},${CMAKE_SHARED_LIBRARY_SUFFIX}>)
-
-set(MV_LINK_LIBRARY "${MV_LINK_PATH}/${CMAKE_SHARED_LIBRARY_PREFIX}MV_Public${MV_LINK_SUFFIX}")
-set(POINTDATA_LINK_LIBRARY "${PLUGIN_LINK_PATH}/${CMAKE_SHARED_LIBRARY_PREFIX}PointData${MV_LINK_SUFFIX}") 
-
-target_link_libraries(${TSNE_PLUGIN} PRIVATE "${MV_LINK_LIBRARY}")
-target_link_libraries(${TSNE_PLUGIN} PRIVATE "${POINTDATA_LINK_LIBRARY}")
+target_link_libraries(${TSNE_PLUGIN} PRIVATE ManiVault::Core)
+target_link_libraries(${TSNE_PLUGIN} PRIVATE ManiVault::PointData)
 
 target_link_libraries(${TSNE_PLUGIN} PRIVATE ${OPENGL_LIBRARIES})
 
@@ -93,7 +84,7 @@ if (NOT DEFINED ENV{CI})
         --install ${PROJECT_BINARY_DIR}
         --config $<CONFIGURATION>
         --component PLUGIN_TSNE
-        --prefix ${MV_INSTALL_DIR}/$<CONFIGURATION>
+        --prefix ${ManiVault_INSTALL_DIR}/$<CONFIGURATION>
         --verbose
     )
 endif()
@@ -103,6 +94,6 @@ endif()
 # -----------------------------------------------------------------------------
 # Automatically set the debug environment (command + working directory) for MSVC
 if(MSVC)
-    set_property(TARGET ${TSNE_PLUGIN} PROPERTY VS_DEBUGGER_WORKING_DIRECTORY $<IF:$<CONFIG:DEBUG>,${MV_INSTALL_DIR}/debug,${MV_INSTALL_DIR}/release>)
-    set_property(TARGET ${TSNE_PLUGIN} PROPERTY VS_DEBUGGER_COMMAND $<IF:$<CONFIG:DEBUG>,"${MV_INSTALL_DIR}/debug/ManiVault Studio.exe","${MV_INSTALL_DIR}/release/ManiVault Studio.exe">)
+    set_property(TARGET ${TSNE_PLUGIN} PROPERTY VS_DEBUGGER_WORKING_DIRECTORY $<IF:$<CONFIG:DEBUG>,${ManiVault_INSTALL_DIR}/debug,${ManiVault_INSTALL_DIR}/release>)
+    set_property(TARGET ${TSNE_PLUGIN} PROPERTY VS_DEBUGGER_COMMAND $<IF:$<CONFIG:DEBUG>,"${ManiVault_INSTALL_DIR}/debug/ManiVault Studio.exe","${ManiVault_INSTALL_DIR}/release/ManiVault Studio.exe">)
 endif()
