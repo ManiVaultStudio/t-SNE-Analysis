74ccf6f09983f92cc7fc66d980b84e886acc59f3
Alexander Vieth <vieth.alexander@gmx.net>
2024-09-25 14:29:12 +0200

    Prefer linking lz4 statically

--- cmake/CMakeLinkUtilities.cmake
+++ cmake/CMakeLinkUtilities.cmake
@@ -4,16 +4,20 @@
 
 # Link the flann libraries via target_link_library to the current project
 macro(set_flann_project_link_libraries target)
-    if(TARGET flann::flann_cpp_s)
-        set(FLANN_TARGET flann::flann_cpp_s)
-    elseif(TARGET flann::flann_cpp)
-        set(FLANN_TARGET flann::flann_cpp)
-    elseif(TARGET flann::flann_s)
-        set(FLANN_TARGET flann::flann_s)
-    elseif(TARGET flann::flann)
-        set(FLANN_TARGET flann::flann)
-    else()
-        message(FATAL_ERROR "No Flann target found.")
+
+    # prefer static linking
+    if(NOT FLANN_TARGET)
+        if(TARGET flann::flann_cpp_s)
+            set(FLANN_TARGET flann::flann_cpp_s)
+        elseif(TARGET flann::flann_cpp)
+            set(FLANN_TARGET flann::flann_cpp)
+        elseif(TARGET flann::flann_s)
+            set(FLANN_TARGET flann::flann_s)
+        elseif(TARGET flann::flann)
+            set(FLANN_TARGET flann::flann)
+        else()
+            message(FATAL_ERROR "No Flann target found.")
+        endif()
     endif()
 
     message (STATUS "Linking Flann library " ${FLANN_TARGET})
@@ -33,8 +37,24 @@ macro(set_HDILib_project_link_libraries target)
 endmacro()
 
 macro(set_lz4_project_link_libraries target)
-    MESSAGE( STATUS "Linking lz4")
-    target_link_libraries("${target}" PRIVATE lz4::lz4)
+
+    # prefer static linking
+    if(NOT LZ4_TARGET)
+        if(TARGET LZ4::lz4_static)
+            set(LZ4_TARGET LZ4::lz4_static)
+        elseif(TARGET LZ4::lz4_shared)
+            set(LZ4_TARGET LZ4::lz4_shared)
+        elseif(TARGET lz4::lz4)
+            set(LZ4_TARGET lz4::lz4)
+        elseif(TARGET LZ4::LZ4)
+            set(LZ4_TARGET LZ4::LZ4)
+        else()
+            message(FATAL_ERROR "No LZ4 target found.")
+        endif()
+    endif()
+
+MESSAGE( STATUS "Linking lz4 library " ${LZ4_TARGET})
+    target_link_libraries("${target}" PRIVATE ${LZ4_TARGET})
 endmacro()
 
 # This silences OpenGL deprecation warnings on MacOS
