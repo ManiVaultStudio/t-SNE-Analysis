9a1e1aff79da58bbd6f2acba52b41e11ec419e66
Thomas Kroes <tkroes81@gmail.com>
2025-06-04 09:59:37 +0200

    Auxiliary tasks should not be foreground tasks

--- src/Common/TsneAnalysis.cpp
+++ src/Common/TsneAnalysis.cpp
@@ -530,10 +530,10 @@ void TsneAnalysis::startComputation()
 
 TsneWorkerTasks::TsneWorkerTasks(QObject* parent, mv::Task* parentTask) :
     QObject(parent),
-    _initializeOffScreenBufferTask(this, "Initialize off-screen GPGPU buffer", Task::GuiScopes{ Task::GuiScope::DataHierarchy, Task::GuiScope::Foreground }, Task::Status::Idle),
-    _computingSimilaritiesTask(this, "Compute similarities", Task::GuiScopes{ Task::GuiScope::DataHierarchy, Task::GuiScope::Foreground }, Task::Status::Idle),
-    _initializeTsneTask(this, "Initialize TSNE", Task::GuiScopes{ Task::GuiScope::DataHierarchy, Task::GuiScope::Foreground }, Task::Status::Idle),
-    _computeGradientDescentTask(this, "Compute gradient descent", Task::GuiScopes{ Task::GuiScope::DataHierarchy, Task::GuiScope::Foreground }, Task::Status::Idle)
+    _initializeOffScreenBufferTask(this, "Initialize off-screen GPGPU buffer", Task::GuiScopes{ Task::GuiScope::DataHierarchy }, Task::Status::Idle),
+    _computingSimilaritiesTask(this, "Compute similarities", Task::GuiScopes{ Task::GuiScope::DataHierarchy }, Task::Status::Idle),
+    _initializeTsneTask(this, "Initialize TSNE", Task::GuiScopes{ Task::GuiScope::DataHierarchy }, Task::Status::Idle),
+    _computeGradientDescentTask(this, "Compute gradient descent", Task::GuiScopes{ Task::GuiScope::DataHierarchy }, Task::Status::Idle)
 {
     _initializeOffScreenBufferTask.setParentTask(parentTask);
     _computingSimilaritiesTask.setParentTask(parentTask);
