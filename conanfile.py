fe898bcc564676bf457ae74f3375d4fd05facd78
Thomas Kroes <t.kroes@lumc.nl>
2025-10-08 15:40:41 +0200

    Implement PDB file copying in package method
    Add functionality to copy PDB files for Windows builds.

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ import shutil
 import pathlib
 import subprocess
 from rules_support import PluginBranchInfo
-
+from conans import tools
+import shutil
 
 class SNEAnalysesConan(ConanFile):
     """Class to package SNE-Analyses using conan
@@ -167,6 +168,17 @@ class SNEAnalysesConan(ConanFile):
                 release_dir,
             ]
         )
+
+        # Add the PDB files to the Conan package
+    	if self.settings.os == "Windows":
+    		print("Copying PDBs...")
+    		pdb_dest = pathlib.Path(package_dir, "RelWithDebInfo", "PDBs")
+    		pdb_dest.mkdir()
+    		pdb_files = pdb_files = [p for p in pathlib.Path(self.build_folder).rglob('*') if p.is_file() and p.suffix.lower() == '.pdb']
+    		print("PDB(s): ", pdb_files)
+    		for pfile in pdb_files:
+    			shutil.copy(pfile, pdb_dest)
+                    
         self.copy(pattern="*", src=package_dir)
 
     def package_info(self):
